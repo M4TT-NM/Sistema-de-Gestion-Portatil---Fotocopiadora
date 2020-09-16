e5f8f724e229313976830f90947e1b88fa04c14d
Manuel <manuelnmatute@gmail.com>
2020-09-16 10:25:06 -0600

    Checker class optimized & new methods added.
    I optimize a little bit this class.
    Also I add a method to set the Business Name and a method to get that name.

--- src/sistema/util/Checker.java
+++ src/sistema/util/Checker.java
@@ -6,16 +6,19 @@
 
 package sistema.util;
 import sistema.db.*;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 /**
- * 
+ * This class is in charge of checking important information for the system.
  * @author Nicolas Matute M.Z <manuelnmatute@gmail.com>
  */
 public class Checker {
 
-    private String KEY = "2WSX1QAZ";
+    private final String KEY = "2WSX1QAZ";
     
     /**
      * This method checks the key wrote by user.
@@ -34,14 +37,13 @@ public class Checker {
                 String key = rs.getString("Data");
                 
                 response = key.equals(KEY);
-                rs.close();
-                st.close();
-                cn.close();
+            
+                SQLoperation.close(rs,st,cn);
+            
             } else {
-                rs.close();
-                st.close();
-                cn.close();
-                
+            
+                SQLoperation.close(rs,st,cn);
+                          
                 String key;
                 do {                    
                     key = JOptionPane.showInputDialog("Ingresa la clave de seguridad:");
@@ -73,9 +75,9 @@ public class Checker {
             
             if(rs.getString("Data") == null){
                 
-                rs.close();
-                st.close();
-                cn.close();
+            
+                SQLoperation.close(rs,st,cn);
+            
                                               
                 String folder_path = JOptionPane.showInputDialog("Ingrese la ruta a la carpeta:");
                 
@@ -107,21 +109,49 @@ public class Checker {
             
             folder_path = rs.getString("Data");
 
-            rs.close();
-            st.close();
-            cn.close();  
-            
+            SQLoperation.close(rs,st,cn);
+                        
         } catch (SQLException e) {
             System.err.println("Error getting reports folder: " + e.getMessage());
         }
         return folder_path;
     }
 
+    /**
+     * This method is used to set the business name in the database.
+     */
+    public void setBusinessName(){
 
+        String businessName = JOptionPane.showInputDialog("Ingrese el nombre de su negocio:");
+
+        SQLoperation.update("PrivateData", "Data = '" + businessName + "'", "Name = 'BusinessName'");
+               
+    }
     
-    
-    
-    
-    
+    /**
+     * This method is used to get the business name registered in the database.
+     * @return the business name. In case of none, returns null.
+     */
+    public String getBusinessName(){
+        String businessName = null;
+        
+        try {
+            Connection cn = Database.connect();
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT Data FROM PrivateData WHERE Name = 'BusinessName'");
+            
+            if(rs.getString("Data") != null){
+                businessName = rs.getString("Data");
+            }
+            
+            SQLoperation.close(rs,st,cn);
+            
+        } catch (SQLException e) {
+            System.err.println("Error getting business name: " + e.getMessage());
+        }
+        
+        return businessName;
+    }
+       
     
 }
